b2b786cdc304097afe8664f55ee2e97abf98844d
vagnercruz <vagnerdacruz48@gmail.com>
2025-06-19 17:55:46 -0300

    Desafio finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws Exception {
+      // TODO: Conhecer e importar a classe scanner
+
+      // Exibir as mensagens papa o usuario
+
+      // obter esse valor e exibir a mensagem conta criada
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Por favor, digite o número da Agência! (Exemplo: 067-8)");
+      String agencia = sc.nextLine();
+
+      System.out.print("Por favor, digite o número da conta: ");
+      int numero = sc.nextInt();
+      sc.nextLine();
+
+      System.out.print("Por favor, digite o seu nome: ");
+      String nomeCliente = sc.nextLine();
+
+      System.out.print("Por favor, digite o valor de saldo que queres adicionar: ");
+      double saldo = sc.nextDouble();
+
+     System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        sc.close();
   } 
 }
